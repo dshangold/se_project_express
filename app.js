81ce3b548751e88373f04d244914f1f23cc970ba
dylanshangold <dylan.shangold@gmail.com>
2025-01-12 18:33:49 -0500

    minor express change

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ const app = express();
 
 const { PORT = 3001 } = process.env;
 
-app.get("/", (req, res) => {
-  res.send("Server is running!");
-});
-
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost.${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
