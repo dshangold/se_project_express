588dfad9067d5bfa9cc4f45029fda909b95c0acf
dylanshangold <dylan.shangold@gmail.com>
2025-01-12 21:20:23 -0500

    make user controller functions

--- controllers/users.js
+++ controllers/users.js
@@ -4,10 +4,45 @@ const User = require("../models/user");
 
 const getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.send(users))
+    .then((users) => res.status(200).send(users))
     .catch((err) => {
       console.error(err);
+      return res.status(500).send({ message: err.message });
     });
 };
 
-module.exports = { getUsers };
+// CREATE USERS //
+
+const createUser = (req, res) => {
+  const { name, avatar } = req.body;
+
+  User.create({ name, avatar })
+    .then((user) => res.status(201).send(user))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "ValidationError") {
+        return res.status(400).send({ message: err.message });
+      }
+      return res.status(500).send({ message: err.message });
+    });
+};
+
+// GET USER BY ID //
+
+const getUser = (req, res) => {
+  const { userId } = req.params;
+  User.findById(userId)
+    .orFail()
+    .then((user) => res.status(200).send(user))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "DocumentNotFound") {
+        // return res.status(400).send({ message: err.message });
+      } else if (err.name === "Cast Error") {
+        // handle the cast error
+      }
+      return res.status(500).send({ message: err.message });
+    });
+};
+
+module.exports = { getUsers, createUser, getUser };
